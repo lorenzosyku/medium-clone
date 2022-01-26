4e516789bd6b95f9213bb8deeaf2c080fd3145f7
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-01-26 00:18:10 -0500

    linked posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home({ posts }: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
-        <title>Create Next App</title>
+        <title>Medium Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -22,20 +22,19 @@ export default function Home({ posts }: Props) {
 
       <Banner />
 
-      {/*TODO: posts */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className="border rounded-lg group cursor-pointer">
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
               <img
                 className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
                 src={urlFor(post.mainImage).url()!}
                 alt=""
               />
-              <div className="">
+              <div className="flex justify-between p-3">
                 <div>
-                  <p>{post.title}</p>
-                  <p>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
                     {post.description} by {post.author.name}
                   </p>
                 </div>
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,76 @@
+import { GetStaticProps } from "next";
+import Header from "../../components/Header";
+import { sanityClient, urlFor } from "../../sanity";
+import { Post } from "../../typing";
+
+interface Props {
+  post: Post
+}
+
+function Post({ post }: Props) {
+  console.log(post)
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
+
+export default Post;
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    slug-> {
+    current
+    }
+  }`;
+  const posts = await sanityClient.fetch(query);
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticprops: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    author-> {
+      name,
+      image,
+    },
+    "comments": *[
+      _type == "comment" &&
+      post._ref == ^._id &&
+      approved == true],
+    description,
+    mainImage,
+    slug,
+    body
+  }`;
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+
+  if(!post){
+    return {
+      notFound: true
+    }
+  }
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60,
+  }
+};
